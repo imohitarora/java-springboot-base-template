455fb7dbf21fd0c3d3489c7f59ad6a7742b2482a
Mohit Arora <me@aboutmohit.com>
2024-02-07 20:51:42 -0500

    refine user entity

--- src/main/java/com/neweltechnologies/portfolio/users/User.java
+++ src/main/java/com/neweltechnologies/portfolio/users/User.java
@@ -8,11 +8,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
+@ToString
 @Entity
 @Audited
 public class User extends BaseEntity {
